2b7bd8a826ad42e05839480ff1aa715422bd9e4a
srivarshithasheelam <srivarshithareddysheelam@gmail.com>
2023-03-25 16:24:49 +0530

    mui installed

---
+++ first/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Badge} from "@mui/material"
+
+function App() {
+  return (
+    <div className="App">
+      <Badge>This is First React file</Badge>
+    </div>
+  );
+}
+
+export default App;
